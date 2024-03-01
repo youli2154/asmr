39a172fd44469ba03a50f1f56971f01466d26232
You Li <91580754+youli2154@users.noreply.github.com>
2024-02-29 20:58:42 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -28,58 +28,34 @@ audio_d.set_volume(volume_d)
 # Main loop
 running = True
 while running:
-    #print(pot.value)
-    volume_a = pot.value
-    audio_a.set_volume(volume_a)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
+                volume_a = pot.value
                 audio_a.play(-1)
-                audio_a.set_volume(volume_a)
             elif event.key == pygame.K_b:
+                volume_b = pot.value
                 audio_b.play(-1)
             elif event.key == pygame.K_c:
+                volume_c = pot.value
                 audio_c.play(-1)
             elif event.key == pygame.K_d:
+                volume_d = pot.value
                 audio_d.play(-1)
                 
-            elif event.key == pygame.K_i:
-                volume_a = pot.value
-                audio_a.set_volume(volume_a)
-                print(volume_a)
-            elif event.key == pygame.K_j:
-                volume_a = max(0.0, volume_a - 0.05)
-                audio_a.set_volume(volume_a)
-
-            elif event.key == pygame.K_m:
-                volume_b = min(1.0, volume_b + 0.05)
-                audio_b.set_volume(volume_b)
-            elif event.key == pygame.K_n:
-                volume_b = min(1.0, volume_b - 0.05)
-                audio_b.set_volume(volume_b)
-
-            elif event.key == pygame.K_t:
-                volume_c = min(1.0, volume_c + 0.05)
-                audio_c.set_volume(volume_c)
-            elif event.key == pygame.K_y:
-                volume_c = max(0.0, volume_c - 0.05)
-                audio_c.set_volume(volume_c)
-
-            elif event.key == pygame.K_u:
-                volume_d = min(1.0, volume_d + 0.05)
-                audio_d.set_volume(volume_d)
-            elif event.key == pygame.K_v:
-                volume_d = max(0.0, volume_d - 0.05)
-                audio_d.set_volume(volume_d)
-
             elif event.key == pygame.K_z:
                 audio_a.set_volume(0)
                 audio_b.set_volume(0)
                 audio_c.set_volume(0)
+
+    # Set volume for each audio based on potentiometer value
+    audio_a.set_volume(volume_a)
+    audio_b.set_volume(volume_b)
+    audio_c.set_volume(volume_c)
+    audio_d.set_volume(volume_d)
 
 # Quit Pygame
 pygame.quit()

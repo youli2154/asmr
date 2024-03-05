c083f53ec330225b01b0f7728291e1d186f82414
You Li <91580754+youli2154@users.noreply.github.com>
2024-03-05 09:55:29 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,7 +12,12 @@ audio_files = {
     pygame.K_a: pygame.mixer.Sound("light-rain.mp3"),
     pygame.K_b: pygame.mixer.Sound("campfire.mp3"),
     pygame.K_c: pygame.mixer.Sound("thunder-2.mp3"),
-    pygame.K_d: pygame.mixer.Sound("Instrument of Surrender.mp3")
+    pygame.K_d: pygame.mixer.Sound("Instrument of Surrender.mp3"),
+    pygame.K_e: pygame.mixer.Sound("wave.mp3"),
+    pygame.K_f: pygame.mixer.Sound("wind.mp3"),
+    pygame.K_g: pygame.mixer.Sound("birds.mp3"),
+    pygame.K_h: pygame.mixer.Sound("cricket.mp3"),
+    pygame.K_i: pygame.mixer.Sound("train.mp3")
 }
 
 # Set initial volumes

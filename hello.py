1203b985896213b7311e9a2d8185f41b9604458c
You Li <91580754+youli2154@users.noreply.github.com>
2024-04-12 00:29:23 -0500

    added sounds

--- hello.py
+++ hello.py
@@ -24,31 +24,31 @@ font = pygame.font.Font(None, 24)
 sound_sets = {
     'O': {
         pygame.K_a: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_b: pygame.mixer.Sound("stream.mp3"),
-        pygame.K_c: pygame.mixer.Sound("waves.mp3"),
+        pygame.K_b: pygame.mixer.Sound("waves.mp3"),
+        pygame.K_c: pygame.mixer.Sound("underwater.mp3"),
         pygame.K_d: pygame.mixer.Sound("fire.mp3"),
-        pygame.K_e: pygame.mixer.Sound("thunder-2.mp3"),
+        pygame.K_e: pygame.mixer.Sound("thunder.mp3"),
         pygame.K_f: pygame.mixer.Sound("wind.mp3"),
         pygame.K_g: pygame.mixer.Sound("birds.mp3"),
         pygame.K_h: pygame.mixer.Sound("cricket.mp3"),
-        pygame.K_i: pygame.mixer.Sound("bowl.mp3"),
-        pygame.K_j: pygame.mixer.Sound("cafe.mp3"),
-        pygame.K_k: pygame.mixer.Sound("train.mp3"),
-        pygame.K_l: pygame.mixer.Sound("Instrument of Surrender.mp3")
+        pygame.K_i: pygame.mixer.Sound("frog.mp3"),
+        pygame.K_j: pygame.mixer.Sound("cat.mp3"),
+        pygame.K_k: pygame.mixer.Sound("temple.mp3"),
+        pygame.K_l: pygame.mixer.Sound("muyu.mp3")
     },
     'P': {
-        pygame.K_a: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_b: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_c: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_d: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_e: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_f: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_g: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_h: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_i: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_j: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_k: pygame.mixer.Sound("light-rain.mp3"),
-        pygame.K_l: pygame.mixer.Sound("light-rain.mp3")
+        pygame.K_a: pygame.mixer.Sound("cafe.mp3"),
+        pygame.K_b: pygame.mixer.Sound("train.mp3"),
+        pygame.K_c: pygame.mixer.Sound("bar.mp3"),
+        pygame.K_d: pygame.mixer.Sound("typewriter.mp3"),
+        pygame.K_e: pygame.mixer.Sound("pencil.mp3"),
+        pygame.K_f: pygame.mixer.Sound("clock.mp3"),
+        pygame.K_g: pygame.mixer.Sound("subway.mp3"),
+        pygame.K_h: pygame.mixer.Sound("arcade.mp3"),
+        pygame.K_i: pygame.mixer.Sound("conveniencestore.mp3"),
+        pygame.K_j: pygame.mixer.Sound("chimes.mp3"),
+        pygame.K_k: pygame.mixer.Sound("footstep.mp3"),
+        pygame.K_l: pygame.mixer.Sound("muyu.mp3")
     }
 }
 
